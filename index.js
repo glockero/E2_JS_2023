ac5f669cfc95b561e6e81ce13f94ba4837f2243c
Gustavo Galarza <galarzagus@outlook.com>
2023-04-23 14:50:27 -0300

    tp2
    tp2

---
+++ index.js
@@ -0,0 +1,110 @@
+const pizzas = [
+  {
+    id: 1,
+    nombre: "pizza de Muzzarella",
+    precio: 500,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
+  },
+
+  {
+    id: 2,
+    nombre: "pizza de Cebolla",
+    precio: 1500,
+    ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
+  },
+
+  {
+    id: 3,
+    nombre: "pizza Napolitana",
+    precio: 1350,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas"],
+  },
+
+  {
+    id: 4,
+    nombre: "pizza 4 Quesos",
+    precio: 1380,
+    ingredientes: [
+      "Muzzarella",
+      "Tomate",
+      "Queso Azul",
+      "Parmesano",
+      "Roquefort",
+    ],
+  },
+
+  {
+    id: 5,
+    nombre: "pizza Especial",
+    precio: 1000,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas", "Cebolla"],
+  },
+
+  {
+    id: 6,
+    nombre: "pizza con Anana",
+    precio: 600,
+    ingredientes: ["Muzzarella", "Tomate", "Anana"],
+  },
+];
+
+
+/* ¿Que debén hacer?
+
+🔥 Utilizando métodos de array e iterando sobre el array de pizzas, realizar las siguientes actividades, imprimiendo en consola:
+
+a)  Las pizzas que tengan un id impar.
+b) Responder: ¿Hay alguna pizza que valga menos de $600?
+c) El nombre de cada pizza con su respectivo precio.
+d) Todos los ingredientes de cada pizza (En cada iteración imprimir los ingredientes de la pizza que se esta recorriendo). Ayuda: van a tener que realizar dos recorridos, ya que cada pizza del array de pizzas tiene una propiedad "ingredientes" cuyo valor es un array con ingredientes.
+
+TODAS  las respuestas deben ser USER-FRIENDLY. 
+Si (como en el punto B), la respuesta es un booleano (true o false, hay o no hay), no imprimir el booleano , imprimir en consola una respuesta que toda persona pueda entender, sepa o no de programación (Es decir, no podemos imprimir un array o un objeto en consola, por ejemplo).
+
+
+ */
+
+//EJERCICO A
+console.log("....EJERCICIO A....");
+console.log("Las pizzas con ID impar son:"); 
+pizzas.forEach(pizza => {
+
+  if (pizza.id %2 != 0) {
+    
+  console.log(pizza.nombre+" "+"con el ID"+" "+"("+pizza.id+")");
+  }
+  
+});
+
+
+
+//EJERCICIO B
+console.log("....EJERCICIO B....");
+console.log("La o las pizzas que valen menos de $600 son:"); 
+
+pizzas.forEach(pizza => {
+
+  if (pizza.precio <= 600) {
+    
+  console.log(pizza.nombre+""+""+" ("+"$"+pizza.precio+")");
+  }
+  
+});
+
+
+//EJERCICIO C
+console.log("....EJERCICIO C....");
+console.log("Estos son los nombre de las pizzas con su precio:"); 
+pizzas.forEach(pizza => {
+
+  console.log("La pizza"+" "+pizza.nombre+" tiene un precion de: "+"$"+pizza.precio);
+});
+
+//EJERCICIO D
+console.log("....EJERCICIO D....");
+console.log("Los ingredientes de cada pizza son:"); 
+pizzas.forEach(pizza => {
+
+  console.log("La pizza"+" "+pizza.nombre+" tiene los sigueintes ingredientes: ");
+  console.log(pizza.ingredientes);
+});
